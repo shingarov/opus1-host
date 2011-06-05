2aaf29e49a6e4b9fb67c2dc1c90b2595a0a35ff4
Boris Shingarov <b1@shingarov.com>
2011-06-05 18:56:49 -0400

    Added -debug argument

--- drawknob.c
+++ drawknob.c
@@ -4,7 +4,12 @@
 #include "hauptwerk.h"
 #include "serial_io.h"
 
+static int DEBUG=0;
+
 static void organ(unsigned int organNo) {
+  if (DEBUG) {
+    printf("  -> organ N.%d\n", organNo);
+  }
   send_short_message(ORGAN_SELECTION_CHANNEL,
                      NOTE_ON,
                      organNo,
@@ -12,6 +17,10 @@ static void organ(unsigned int organNo) {
 }
 
 static void coupler(unsigned int couplerNo) {
+  if (DEBUG) {
+    printf("  -> coupler N.%d\n", couplerNo);
+  }
+
   send_short_message(COUPLER_CHANNEL,
                      NOTE_ON,
                      couplerNo,
@@ -19,6 +28,9 @@ static void coupler(unsigned int couplerNo) {
 }
 
 static void speaking_stop(unsigned int x, unsigned int y) {
+  if (DEBUG) {
+    printf("  -> speaking stop %dx%d\n", x, y);
+  }
   send_short_message(SPEAKING_STOP_CHANNEL,
                      NOTE_ON,
                      (x<<4) | y,
@@ -68,13 +80,34 @@ void process_raw_drawknob_input(unsigned char c) {
   x = (c&0xF0) >> 4;
   y = c&0x0F;
 
+  if (DEBUG) {
+    printf("\nraw drawknob input: %X\n", c);
+  }
+
   if (y==0) organ(x);
   else if (y==0x0A) coupler(x);
   else if ((y>=1) && (y<=9)) speaking_stop(x, y);
   else CRASH("Bad drawknob input");
 }
 
-int main() {
+static void process_argument(char *arg) {
+  if (!strcmp(arg, "-debug")) {
+    DEBUG=1;
+    return;
+  }
+  CRASH(strcat("BAD ARGUMENT ON COMMAND LINE: ", arg));
+}
+
+static void process_arguments(int argc, char **argv) {
+  int i;
+  for (i=1; i<argc; i++) {
+    process_argument(argv[i]);
+  }
+}
+
+int main(int argc, char **argv) {
+  process_arguments(argc, argv);
+
   open_serial_port(KNOB_COM_PORT);
   init_midi_out(MIDI_PORT_DRAWKNOBS_OPUS1_TO_HAUPTWERK);
   start_hauptwerk();

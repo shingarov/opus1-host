155a0d9e935838ea429de04074122b472caaf42d
Boris Shingarov <b1@shingarov.com>
2011-07-30 23:47:25 -0400

    Added command-line debug utils for LED

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 cl LED.c LED_main.c CRASH.c MIDI.c serial_io.c winmm.lib
 cl drawknob.c CRASH.c MIDI.c serial_io.c winmm.lib
+cl Debug_LED.c CRASH.c serial_io.c LED.c
+cl Debug_LED_2.c CRASH.c serial_io.c LED.c
 

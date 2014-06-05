3c89946dd981b73f5a42112a753e2f277b136cce
Boris Shingarov <b1@shingarov.com>
2014-06-05 05:10:24 -0400

    Upgraded console LED debugging capability as we now have drawknob LEDs and 7segment LEDs, instead of combination LEDs

--- Debug_console_LED.c
+++ Debug_console_LED.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2011 LADAREVO SOFTWARE INC.
+ * Copyright (c) 2009-2014 LADAREVO SOFTWARE INC.
  * LADAREVO ORGELBAU LAB
  */
 
@@ -9,34 +9,70 @@
 #include "serial_io.h"
 
 static void usage() {
-  printf("Usage: L cmd,\n");
-  printf("  L = 0(M2), 1-7, 8(BASS), 9(MELODY), A(CHIMES)\n");
+  printf("Usage: DXY on|off\n");
 }
 
 static void act_on(char *command) {
   int on_off;
-  char X;
-  int x;
-  if (strlen(command)!=4) {
-    usage();
-    return;
+  int d;
+  int x, y;
+  if (strlen(command)==6) {
+    if (strcmp(command+3, " on")) {
+      usage();
+      return;
+    }
+    on_off=ON;
+  } else {
+    if (strcmp(command+3, " off")) {
+      usage();
+      return;
+    }
+    on_off=OFF;
   }
-  if (!strcmp(command+1, " on")) on_off=ON;
-  else if (!strcmp(command+1, " off")) on_off=OFF;
-  else {
-    printf("cmd should be \"on\" or \"off\"\n");
-    return;
+
+  x = *(command+1) - '0';
+  y = *(command+2) - '0';
+
+  d = *command - '0';
+  switch(d) {
+    case 0:
+      setConsoleStopLED(x,y,on_off); return;
+    case 1:
+      set7SegmentLED(x,y,on_off); return;
+    default:
+      usage();
+      return;
   }
-  X = *command;
-  x = X-'0';
-  if (X=='A') x=10;
-  setConsoleLed(x, on_off);
+}
+
+unsigned char sevenSegmentFont[] = {250, 192, 182,214, 204,94,126,194,254,222};
+
+void set7segmentDigit(unsigned digitNo, int value, int dp) {
+  unsigned char mask = (value==-1)? 0:sevenSegmentFont[value];
+  int i;
+  for (i=0; i<=7; i++) {
+    set7SegmentLED(digitNo, i, (mask>>i)&1);
+  }
+  set7SegmentLED(digitNo, 0, dp);
+}
+
+void test7segment() {
+  set7segmentDigit(0,1,1);
+  set7segmentDigit(1,2,0);
+  set7segmentDigit(2,3,0);
+  set7segmentDigit(3,4,0);
+  set7segmentDigit(4,5,0);
+  set7segmentDigit(5,6,0);
+  set7segmentDigit(6,7,0);
+  set7segmentDigit(7,8,0);
+
 }
 
 int main() {
   open_serial_port(RODGERS_COM_PORT);
   for (;;) {
     char buf[128];
+    test7segment();
     gets(buf);
     act_on(buf);
   }
